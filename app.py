abb15341effc9b2f85451c3301adb732babfc788
roccoKP <rocco.caputo@keypartner.it>
2018-04-18 18:30:35 +0200

    azure2

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ logging.basicConfig(level=logging.DEBUG)
 
 print ("start")
 
-client = MongoClient("mongodb://kp-devops:TlZmBPJBNrSCWkawD31t5IbR9pyfiIxufiINEVkAHijEtJ8NwT6ZfofqQUYP1NrQK0ZEFS8zPysIOr0u1p2xwA==@kp-devops.documents.azure.com:10255/?ssl=true&replicaSet=globaldb",ssl_cert_reqs=ssl.CERT_NONE) #host uri
-db = client.mongoChat    #Select the database
-db.authenticate(name="kp-devops",password='TlZmBPJBNrSCWkawD31t5IbR9pyfiIxufiINEVkAHijEtJ8NwT6ZfofqQUYP1NrQK0ZEFS8zPysIOr0u1p2xwA==')
+#client = MongoClient("mongodb://kp-devops:TlZmBPJBNrSCWkawD31t5IbR9pyfiIxufiINEVkAHijEtJ8NwT6ZfofqQUYP1NrQK0ZEFS8zPysIOr0u1p2xwA==@kp-devops.documents.azure.com:10255/?ssl=true&replicaSet=globaldb",ssl_cert_reqs=ssl.CERT_NONE) #host uri
+#db = client.mongoChat    #Select the database
+#db.authenticate(name="kp-devops",password='TlZmBPJBNrSCWkawD31t5IbR9pyfiIxufiINEVkAHijEtJ8NwT6ZfofqQUYP1NrQK0ZEFS8zPysIOr0u1p2xwA==')
 
 print ("conn che non serve creata")
 
@@ -27,17 +27,17 @@ print ("conn che non serve creata")
 #db.authenticate(name=os.getenv("MONGO_USERNAME"),password=os.getenv("MONGO_PASSWORD"))
 #todos = db.todo #Select the collection
 
-english_bot = ChatBot(
-    'Mongoloide',
-    storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
-    database='mongoChat',
+#english_bot = ChatBot(
+    #'Mongoloide',
+    #storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
+    #database='mongoChat',
     #database_uri='mongodb://172.17.0.2:27017/',
     #database_uri='mongodb://kp-devops:TlZmBPJBNrSCWkawD31t5IbR9pyfiIxufiINEVkAHijEtJ8NwT6ZfofqQUYP1NrQK0ZEFS8zPysIOr0u1p2xwA==@kp-devops.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
     #database_uri='mongodb://kp-devops:TlZmBPJBNrSCWkawD31t5IbR9pyfiIxufiINEVkAHijEtJ8NwT6ZfofqQUYP1NrQK0ZEFS8zPysIOr0u1p2xwA==@kp-devops.documents.azure.com:10255/?ssl=true&replicaSet=globaldb&ssl_ca_certs=/Users/rocco/Downloads/TLSCA5.pem',
-    database_uri=os.getenv("MONGOLOIDE")
+    #database_uri=os.getenv("MONGOLOIDE")
     #trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
-    trainer='chatterbot.trainers.ListTrainer'
-)
+    #trainer='chatterbot.trainers.ListTrainer'
+#)
 
 #english_bot.set_trainer(ChatterBotCorpusTrainer)
 #english_bot.train("chatterbot.corpus.english")
@@ -46,10 +46,10 @@ english_bot = ChatBot(
 def home():
     return render_template("index.html")
 
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+#@app.route("/get")
+#def get_bot_response():
+#    userText = request.args.get('msg')
+#    return str(english_bot.get_response(userText))
 
 
 if __name__ == "__main__":
